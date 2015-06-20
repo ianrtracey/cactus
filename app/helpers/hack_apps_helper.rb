8eb1bd1136f3ba61c5f258987a1f7c8677c2fa2a
ianrtracey@gmail.com <ianrtracey@gmail.com>
2015-06-19 22:19:58 -0700

    Adds framework for hack_app show view

--- app/helpers/hack_apps_helper.rb
+++ app/helpers/hack_apps_helper.rb
@@ -1,2 +1,37 @@
 module HackAppsHelper
+	def pretty_status(status)
+		if status == "Undecided"
+		  return '<span class="ui yellow label">Undecided</span>'.html_safe
+		elsif status == "Approved"
+		  return '<span class="ui yellow label">Approved</span>'.html_safe
+		else
+		  return '<span class="ui yellow label">Denied</span>'.html_safe
+		end
+	end
+
+	def pretty_travel_reimbursement(does_request)
+		if does_request
+		  return '<span class="ui blue label">Yes</span>'.html_safe
+		else
+ 		  return '<span class="ui label">No</span>'.html_safe 
+ 		end
+	end
+
+	def pretty_first_time_hacker(is_first_time)
+		if is_first_time
+		  return '<span class="ui label">Yes</span>'.html_safe
+		else
+ 		  return '<span class="ui label">No</span>'.html_safe 
+ 		end
+	end
+
+	def pretty_status_ribbon(status)
+		if status == "Undecided"
+		  return '<a class="ui huge yellow right ribbon label">Undecided</a>'.html_safe
+		elsif status == "Approved"
+		  return '<a class="ui huge green right ribbon label">Approved</a>'.html_safe
+		else
+		  return '<a class="ui huge red right ribbon label">Denied</a>'.html_safe
+		end
+	end
 end
